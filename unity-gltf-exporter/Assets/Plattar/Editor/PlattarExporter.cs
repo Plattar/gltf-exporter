6be20ba89df4ffd2016fbdbfe9c5ba80bdaecb10
David Arayan <david.arayan12@gmail.com>
2019-03-13 15:26:46 +1100

    - Added proper Zip support to export the GLTF and Zip automatically

--- unity-gltf-exporter/Assets/Plattar/Editor/PlattarExporter.cs
+++ unity-gltf-exporter/Assets/Plattar/Editor/PlattarExporter.cs
@@ -7,11 +7,13 @@ using UnityGLTF;
 using UnityEngine.SceneManagement;
 using System.IO;
 using System.Text.RegularExpressions;
+using System;
 
 namespace Plattar {
 	public class PlattarExporter : EditorWindow {
 		
 		AnimBool showSettings;
+		AnimBool showExportSettings;
 		GameObject selectedObject;
 		static Texture logo;
 		
@@ -27,6 +29,9 @@ namespace Plattar {
 		void OnEnable() {
 			showSettings = new AnimBool(false);
 			showSettings.valueChanged.AddListener(Repaint);
+			
+			showExportSettings = new AnimBool(false);
+			showExportSettings.valueChanged.AddListener(Repaint);
 		}
 		
 		void OnGUI() {
@@ -77,32 +82,85 @@ namespace Plattar {
 				
 				EditorGUILayout.Separator();
 				EditorGUILayout.BeginVertical();
+				
+				showExportSettings.target = EditorGUILayout.ToggleLeft("Generator Options", showExportSettings.target);
+			
+				//Extra block that can be toggled on and off.
+				if (EditorGUILayout.BeginFadeGroup(showExportSettings.faded)) {
+					EditorGUI.indentLevel++;
+					
+					EditorGUILayout.BeginVertical();
+					PlattarExporterOptions.IsZip = EditorGUILayout.Toggle("Export Zip Archive", PlattarExporterOptions.IsZip);
+					EditorGUILayout.EndVertical();
+					EditorGUI.indentLevel--;
+				}
+				
+				EditorGUILayout.EndFadeGroup();
 
 				if (GUILayout.Button("Export " + selectionName + " to GLTF")) {
-					var exporter = new GLTFSceneExporter(new Transform[] { selectedObject.transform }, RetrieveTexturePath);
-
-					var path = EditorUtility.OpenFolderPanel("glTF Export Path", "", "");
-					if (!string.IsNullOrEmpty(path)) {
-						path += "/" + selectionName + "_export_gltf";
-						DirectoryInfo info = Directory.CreateDirectory(path);
-						
-						if (info.Exists) {
-							exporter.SaveGLTFandBin(path, selectionName);
+					if (!PlattarExporterOptions.IsZip) {
+						if (GenerateGLTF(selectedObject) != null) {
 							EditorUtility.DisplayDialog("Successful Export", "GLTF Exported Successfully", "OK");
-						}
-						else {
-							EditorUtility.DisplayDialog("Failed Export", "GLTF Could not be exported, could not create export path", "OK");
 						}
 					}
 					else {
-						EditorGUILayout.HelpBox("Failed to export since the path is invalid", MessageType.Error);
-						EditorUtility.DisplayDialog("Failed Export", "GLTF Could not be exported, invalid export path provided", "OK");
+						if (GenerateGLTFZipped(selectedObject) != null) {
+							EditorUtility.DisplayDialog("Successful Export", "GLTF Exported and Zipped Successfully", "OK");
+						}
 					}
 				}
+				
 				EditorGUILayout.EndVertical();
 			}
 		}
 		
+		/**
+		 * Generate the GLTF file and zip it all up
+		 */
+		public static Tuple<string, string> GenerateGLTFZipped(GameObject selectedObject) {
+			var path = GenerateGLTF(selectedObject);
+			
+			if (path == null) {
+				return path;
+			}
+			
+			// otherwise, we need to zip up the entire directory
+			// and delete the original
+			string selectionName = selectedObject.name;
+			PlattarExporterOptions.CompressDirectory(path.Item2, path.Item1 + "/" + selectionName);
+			PlattarExporterOptions.DeleteDirectory(path.Item2);
+
+			return path;
+		}
+
+		/**
+		 * Generate a non-zipped GLTF file with all folders etc
+		 */
+		public static Tuple<string, string> GenerateGLTF(GameObject selectedObject) {
+			var exporter = new GLTFSceneExporter(new Transform[] { selectedObject.transform }, RetrieveTexturePath);
+			string selectionName = selectedObject.name;
+
+			var path = EditorUtility.OpenFolderPanel("glTF Export Path", "", "");
+			if (!string.IsNullOrEmpty(path)) {
+				var newpath = path + "/" + selectionName + "_export_gltf";
+				DirectoryInfo info = Directory.CreateDirectory(newpath);
+				
+				if (info.Exists) {
+					exporter.SaveGLTFandBin(newpath, selectionName);
+
+					return new Tuple<string, string>(path, newpath);
+				}
+				
+				EditorUtility.DisplayDialog("Failed Export", "GLTF Could not be exported, could not create export path", "OK");
+			}
+			else {
+				EditorGUILayout.HelpBox("Failed to export since the path is invalid", MessageType.Error);
+				EditorUtility.DisplayDialog("Failed Export", "GLTF Could not be exported, invalid export path provided", "OK");
+			}
+
+			return null;
+		}
+
 		public static string RetrieveTexturePath(UnityEngine.Texture texture) {
 			return AssetDatabase.GetAssetPath(texture);
 		}
---
+++ unity-gltf-exporter/Assets/Plattar/PlattarExporterOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Plattar {
+	public class PlattarExporterOptions {
+		public static readonly string ZipExtension = ".zip";
+		public static bool IsZip = true;
+		
+		public static void CompressDirectory(string source, string destination) {
+			// Compress a folder.
+			ZipFile.CreateFromDirectory(source, destination + ZipExtension);
+		}
+		
+		public static void DeleteDirectory(string target_dir) {
+			string[] files = Directory.GetFiles(target_dir);
+			string[] dirs = Directory.GetDirectories(target_dir);
+			
+			foreach (string file in files) {
+				File.SetAttributes(file, FileAttributes.Normal);
+				File.Delete(file);
+			}
+			
+			foreach (string dir in dirs) {
+				DeleteDirectory(dir);
+			}
+			
+			Directory.Delete(target_dir, false);
+		}
+	}
+}
